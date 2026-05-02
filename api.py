9544307d0736751f0aa6582d05bb14b57dbdb1b1
BetulBNY <betulbny@gmail.com>
2026-05-02 18:11:49 +0300

    bugs in api fiel fixed

--- api.py
+++ api.py
@@ -18,12 +18,11 @@ def get_cryptos():
     else:
         raise HTTPException(status_code=404, detail="Data file not found")  
     
-
-@app.get("cryptos/expensive")
+@app.get("/cryptos/expensive")
 def get_expensive_cryptos():
     if not os.path.exists(DATAPATH):
-        raise HTTPException(status_code=404, detail="Data file not found"),
+        raise HTTPException(status_code=404, detail="Data file not found")
         
     df = pd.read_csv(DATAPATH)
-    expensice_cryptos = df[df['current_price'] >= 1000]
-    return expensice_cryptos.to_dict(orient="records")
+    expensive_cryptos = df[df['current_price'] >= 1000]
+    return expensive_cryptos.to_dict(orient="records")
